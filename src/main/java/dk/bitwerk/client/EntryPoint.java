50147b6baabe4c8b764f2afee050449592215b83
cristcost <cristiano.costantini@gmail.com>
2015-09-17 08:14:29 +0200

    added some logging to outline script loading timing, moved call to GWT code after 3 seconds from page load

--- src/main/java/dk/bitwerk/client/EntryPoint.java
+++ src/main/java/dk/bitwerk/client/EntryPoint.java
@@ -1,11 +1,17 @@
 package dk.bitwerk.client;
 
-import com.google.gwt.core.shared.GWT;
-
 public class EntryPoint implements com.google.gwt.core.client.EntryPoint {
 
-	@Override
-	public void onModuleLoad() {
-		GWT.log("Entrypoint loaded ...");
-	}
+  @Override
+  public void onModuleLoad() {
+    // GWT.log("Entrypoint loaded ...");
+    jsLog("GWT Entrypoint loaded ...");
+    
+  }
+
+  // I prefer to log directly on browser console
+  public native void jsLog(String msg) /*-{
+    console.log(msg);
+	}-*/;
+
 }
--- src/main/java/dk/bitwerk/client/JsHello.java
+++ src/main/java/dk/bitwerk/client/JsHello.java
@@ -1,10 +1,15 @@
 package dk.bitwerk.client;
 
 import com.google.gwt.core.client.js.JsExport;
+import com.google.gwt.core.client.js.JsNamespace;
+import com.google.gwt.core.client.js.JsType;
 
-@JsExport("poc.Hello") 
-public class JsHello  {
-	public String sayHello(String name) {
-		return "Hello " + name;
-	}
+@JsExport
+@JsType
+@JsNamespace("poc")
+public class JsHello {
+
+  public String sayHello(String name) {
+    return "Hello " + name;
+  }
 }
